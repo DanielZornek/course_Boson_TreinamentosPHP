be096cb2e6f020455e3518627e9d05e9c892ab98
DanielZornek <daniel_zornek91@outlook.com>
2023-04-07 17:24:16 -0300

    variáveis

---
+++ BasicSintax/variaveis.php
@@ -0,0 +1,10 @@
+<?php
+
+    $nome = "Toya";
+    $idade = 25;
+    echo "O meu nome é " . $nome;
+    // Variáveis superglobais
+    echo "<br>O caminho do arquivo é " . $_SERVER['PHP_SELF'];
+    echo "<br>A pasta do arquivo é " . $_SERVER['DOCUMENT_ROOT'];
+    
+?>

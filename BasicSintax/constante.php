961ec578761033c94048b286ad8db6482ab4fdc2
DanielZornek <daniel_zornek91@outlook.com>
2023-04-08 15:00:21 -0300

    constantes

---
+++ BasicSintax/constante.php
@@ -0,0 +1,12 @@
+<?php
+    print("<h1>Trabalhando com Constante php </h1>");
+    define("nome", "Daniel");
+    define("sobrenome", "Zornek");
+    define("id", 001);
+    define("raca", "humana"); // Serve para ignorar o case da palavra
+
+    echo nome."<br>";
+    echo sobrenome."<br>";
+    echo id."<br>";
+    echo raca."<br>";
+?>

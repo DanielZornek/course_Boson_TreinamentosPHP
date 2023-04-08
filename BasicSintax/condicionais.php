f6237d219e3692a603ffd3af1070ceaabb446872
DanielZornek <daniel_zornek91@outlook.com>
2023-04-08 15:14:22 -0300

    controle de fluxo - condicionais

---
+++ BasicSintax/condicionais.php
@@ -0,0 +1,16 @@
+<?php
+
+    $num = 20;
+
+    if($num < 20){
+        echo "É menor do que 20";
+        if($num == 10){
+            echo "O número é 10";
+        }
+    }else if($num === 20){
+        echo "Ele é exatamente 20";
+    }else{
+        echo "É maior do que 20";
+    }
+
+?>

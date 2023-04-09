065ff612382d051cf2502d370b7d61958c70f50d
DanielZornek <daniel_zornek91@outlook.com>
2023-04-09 02:39:23 -0300

    loop1

---
+++ BasicSintax/whileDoWhile.php
@@ -0,0 +1,14 @@
+<?php
+    $i = 0;
+    while($i <= 10){
+        echo "$i <br>";
+        $i++;
+    }
+
+    $num2 = 0;
+    echo "<br>Tabuada do 5<br>";
+    Do{
+        echo $num2." X "." 5 = ".$num2*5,"<br>";
+        $num2++;
+    }while($num2<=10);
+?>
